81baa4d7c3b308b78e223163540880c5f8ee6c0f
Jack DeVincens <78175287+jackdevincens@users.noreply.github.com>
2023-01-19 12:28:29 -0500

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Jack Marshall DeVincens on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
